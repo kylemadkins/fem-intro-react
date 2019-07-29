db6bad2f90e212c4265573c566c1fdb73489b5c0
Kyle Adkins <kyle.m.adkins@gmail.com>
2019-07-29 15:20:33 -0500

    Add prettier and gitignore

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,20 +1,24 @@
 const Pet = ({ name, animal, breed }) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, name),
-    React.createElement('h2', {}, animal),
-    React.createElement('h2', {}, breed)
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
   ]);
 };
 
 const App = () => {
-  return React.createElement(
-    'div',
-    {},
-    [
-      React.createElement('h1', {}, 'Adopt Me!'),
-      React.createElement(Pet, { name: 'Hudson', animal: 'Dog', breed: 'Goldendoodle' }),
-      React.createElement(Pet, { name: 'Ollie', animal: 'Cat', breed: 'Tabby' }),
-      React.createElement(Pet, { name: 'Sterling', animal: 'Cat', breed: 'Domestic Longhair' })
-    ]
-  );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Hudson",
+      animal: "Dog",
+      breed: "Goldendoodle"
+    }),
+    React.createElement(Pet, { name: "Ollie", animal: "Cat", breed: "Tabby" }),
+    React.createElement(Pet, {
+      name: "Sterling",
+      animal: "Cat",
+      breed: "Domestic Longhair"
+    })
+  ]);
 };
