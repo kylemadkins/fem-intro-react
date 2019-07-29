e84b4ab6490d5d0e9ab0068fa20d0c7213a233f6
Kyle Adkins <kyle.m.adkins@gmail.com>
2019-07-29 17:06:01 -0500

    Allow user to select type of animal

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog");
+
   return (
     <div className="search-params">
       <form>
@@ -13,6 +16,22 @@ const SearchParams = () => {
             placeholder="Location..."
             onChange={e => setLocation(e.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={e => setAnimal(e.target.value)}
+            onBlur={e => setAnimal(e.target.value)}
+          >
+            <option>Select</option>
+            {ANIMALS.map(a => (
+              <option key={a} value={a}>
+                {a}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
