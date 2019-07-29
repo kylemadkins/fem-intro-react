17317d187b744272ccc8cad05b916bdb29aa7ad5
Kyle Adkins <kyle.m.adkins@gmail.com>
2019-07-29 15:05:38 -0500

    Plain JavaScript example

---
+++ adopt-me/src/App.js
@@ -0,0 +1,20 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, name),
+    React.createElement('h2', {}, animal),
+    React.createElement('h2', {}, breed)
+  ]);
+};
+
+const App = () => {
+  return React.createElement(
+    'div',
+    {},
+    [
+      React.createElement('h1', {}, 'Adopt Me!'),
+      React.createElement(Pet, { name: 'Hudson', animal: 'Dog', breed: 'Goldendoodle' }),
+      React.createElement(Pet, { name: 'Ollie', animal: 'Cat', breed: 'Tabby' }),
+      React.createElement(Pet, { name: 'Sterling', animal: 'Cat', breed: 'Domestic Longhair' })
+    ]
+  );
+};
