1be8f631e66f212ead8fda70d6033b45fe63b7bd
Kyle Adkins <kyle.m.adkins@gmail.com>
2019-07-29 16:53:23 -0500

    Create SearchParams component with hook

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet.js";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Hudson" animal="Dog" breed="Goldendoodle" />
-      <Pet name="Ollie" animal="Cat" breed="Tabby" />
-      <Pet name="Sterling" animal="Cat" breed="Domestic Longhair" />
+      <SearchParams />
     </div>
   );
 };
