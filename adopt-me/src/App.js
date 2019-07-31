2284225de86357d06a6308537017ce16e8dc96c1
Kyle Adkins <kyle.m.adkins@gmail.com>
2019-07-30 23:08:54 -0500

    Add useContext example for shared state management

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import { Router, Link } from "@reach/router";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
+  const themeHook = useState("darkblue");
   return (
     <div>
-      <header>
-        <Link to="/">Adopt Me!</Link>
-      </header>
-      <Router>
-        <SearchParams path="/" />
-        <Details path="/details/:id" />
-      </Router>
+      <ThemeContext.Provider value={themeHook}>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </ThemeContext.Provider>
     </div>
   );
 };
